43907857e5aad254ed5876973fd99a44c8fb6d19
Om Rokade <Omrokade904@gmail.com>
2026-03-30 12:16:50 +0530

    changes in token setting

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -42,7 +42,11 @@ async function registerUserController(req,res){
         {expiresIn: "1d"}
     )
 
-    res.cookie("token",token);
+    res.cookie("token",token,{
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        sameSite: "none"
+    });
 
     res.status(201).json({
         message: "User registered Successfully",
@@ -84,7 +88,11 @@ async function loginUserController(req,res){
         {expiresIn: "1d"}
     )
 
-    res.cookie("token",token);
+    res.cookie("token",token,{
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        sameSite: "none"
+    });
     res.status(200).json({
         message: "User logged in successfully",
         user:{
