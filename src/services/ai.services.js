7bf4a841b75922cf7dcc8320edc67286c7436c8a
Om Rokade <Omrokade904@gmail.com>
2026-04-06 15:31:32 +0530

    resume pdf genration optimized

--- src/services/ai.services.js
+++ src/services/ai.services.js
@@ -166,7 +166,13 @@ async function generateInterviewReport({ resume, selfDescription, jobDescription
 }
 
 async function generatePdfFromHtml(htmlContent) {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({
+    headless: "new",
+    args: [
+      "--no-sandbox",
+      "--disable-setuid-sandbox"
+    ]
+  });
   const page = await browser.newPage();
   await page.setContent(htmlContent, { waitUntil: "networkidle0" })
 
